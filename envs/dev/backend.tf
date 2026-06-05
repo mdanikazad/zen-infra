c4dc5bfd12675ee32c0e760c0ab4a97f65a96771
Onik Azad <onikazadovey@gmail.com>
2026-06-05 16:15:16 +0800

    added a dummy comment at the first line

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -2,7 +2,7 @@
 
 terraform {
   backend "s3" {
-    bucket       = "zen-pharma-terraform-state-mdanikazad"  
+    bucket       = "zen-pharma-terraform-state-mdonikazad"  
     key          = "envs/dev/terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
